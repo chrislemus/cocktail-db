36accedf5e33e9e508af42a13f0cad6ee4a54a45
chrislemus <icrislemus@gmail.com>
2021-01-07 11:54:28 -0500

    most navigation is completed - 75% done

--- lib/cocktail_db/cli.rb
+++ lib/cocktail_db/cli.rb
@@ -1,22 +1,26 @@
 require 'pry'
 class CocktailDb::CLI
   URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=m"
-  # attr_reader :header
-  # def initialize
-    
-  # end
+  attr_reader :results_per_page
+  def initialize
+    @results_per_page = 10
+  end
   def get_tipsy
     puts 'WELCOME TO COCKTAIL DB!'.cyan.blink
     CocktailDb::Data.new.create_drinks  
     user_input = nil
     print_header 
     until user_input == 'exit'
+      
       user_input = gets.strip
-      print_header if !user_input.to_i.between?(1,3)
+      main_method_called = user_input.to_i.between?(2,5)
+      print_header if !main_method_called
       case user_input
       when '1';  get_drinks_count
-      when '2';  list_all_drinks; print_header
-      when '3';  search_for_drink; print_header
+      when '2';  list_all_drinks; 
+      when '3';  search_for_drink; 
+      when '4'; list_all_drink_categories; 
+      when '5'; list_all_drink_glass_types
       when 'menu'; print_navigation
       else
         if user_input == 'exit'
@@ -25,6 +29,7 @@ class CocktailDb::CLI
           p('invalid input', 'red') 
         end
       end
+      print_header if main_method_called
     end
   end
 
@@ -42,7 +47,8 @@ class CocktailDb::CLI
     p(sub_head) if sub_head
   end
 
-  def p(string, text_color = 'cyan') #prints string with styling
+  #prints string with styling
+  def p(string, text_color = 'cyan') 
     string = string.to_s if !string.is_a? String
     puts string.send(text_color)
   end
@@ -51,49 +57,79 @@ class CocktailDb::CLI
     p('1 - Get drink count')
     p('2 - List all drinks')
     p('3 - Search for drink')
+    p('4 - list all drink categories')
+    p('5 - list all glass types for drinks')
     p('exit - exits program')
   end
 
   def list_all_drinks
-    drinks = get_sorted_drinks.map.with_index {|drink, idx| "#{idx+1}. #{drink.name}"}
-    page_results(get_sorted_drinks, "Drinks List", true)
+    drinks = CocktailDb::Drink.all
+    page_results(drinks, "Drinks List", 'drink info')
   end
 
-  def page_results(items_array, results_title, items_are_drinks=false)
+  def list_drinks_with_filter(filter_type, filter_param)
+    drinks = nil
+    case filter_type
+    when 'Glass types'
+      drinks = CocktailDb::Drink.get_drinks_by_glass_type(filter_param)
+    when 'Categories'
+      drinks = CocktailDb::Drink.get_drinks_by_category(filter_param)
+    end
+
+    header = "#{filter_type} ► #{filter_param} ► Drinks"
+    page_results(drinks, header, 'drink info')
+  end
+
+  def page_results(results, results_title, data_type)
     user_input = nil 
-    results_per_page = 10
-    sub_head = 'next - next page | prev - previous page | home - go home'
+    sub_head = "n - next page | p - previous page | bb - go back \n*enter item number to get #{data_type}"
     page_number = 1
-    
-    item_names_numbered_list = items_array.map.with_index {|item, idx| "#{idx+1}. #{item.name}"} 
-    number_of_pages = (items_array.length.to_f/results_per_page.to_f).ceil
-    until user_input == 'home'
-      header = "#{results_title} (#{page_number} of #{number_of_pages})"
-      print_header(false, header, sub_head)
-      p('**enter drink number to get drink info')
-      print_page_results(page_number, results_per_page, item_names_numbered_list)
+    results_text = data_type != 'drink info' ? results : results.map{|drink| drink.name}
+    number_of_pages = (results.length.to_f/@results_per_page.to_f).ceil
+    until user_input == 'bb'
+      print_header(false, results_title, sub_head)
+      print_page_results(page_number, results_text)
+      p("-- PAGE #{page_number} of #{number_of_pages} --", 'cyan')
       user_input = gets.strip
       case user_input
-      when 'next'
+      when 'n'
         (page_number+1).between?(1,number_of_pages) ? page_number+=1 : p('no page to show, please try again', 'red')
-      when 'prev'
+      when 'p'
         (page_number-1).between?(1,number_of_pages) ? page_number-=1 : p('no page to show, please try again', 'red')
       else
         #user has selected a drink
-        if items_are_drinks && user_input.to_i.between?(1,items_array.length)
-          display_drink_info(items_array[(user_input.to_i)-1], results_title)
-        elsif  user_input != 'home'
+        if user_input.to_i.between?(1,results.length)
+          user_selection = results[(user_input.to_i)-1]
+          case data_type
+          when 'drink info'; display_drink_info(user_selection, results_title)
+          when 'drinks by category'; list_drinks_with_filter(results_title, user_selection) 
+          when 'drinks by glass type'; list_drinks_with_filter(results_title, user_selection) 
+          end
+        elsif  user_input != 'bb'
           puts 'invalid input'.red
         end
       end
     end
   end
 
-  def print_page_results(page_number, results_per_page, item_names_numbered_list)
-    first_item_idx = (page_number * results_per_page) - results_per_page
-    current_page_results = item_names_numbered_list.slice(first_item_idx, results_per_page)
-    current_page_results.each{|item| p(item, 'green')}
+  def print_page_results(page_number, results)
+    first_item_idx = (page_number * @results_per_page) - @results_per_page
+    current_page_results = results.slice(first_item_idx, @results_per_page)
+    current_page_results.each_with_index{|item, idx| 
+      item_list_number = first_item_idx + 1 + idx
+      p("#{item_list_number}. #{item}", 'green')
+    }
   end
+
+  def list_all_drink_glass_types
+    page_results(CocktailDb::Drink.get_glass_types, "Glass types", "drinks by glass type")
+  end
+
+  def list_all_drink_categories
+    page_results(CocktailDb::Drink.get_categories, "Categories", "drinks by category")
+  end
+
+
 
   def display_drink_info(drink, header)
     ingredients = drink.ingredients.map{|i| "∙ #{i[0]}: #{i[1]}"}.join("\n")
@@ -110,29 +146,20 @@ class CocktailDb::CLI
     gets.strip
   end
 
-  def get_sorted_drinks
-    CocktailDb::Drink.all.sort { |a, b| a.name <=> b.name }
-  end
 
   def search_for_drink
     header = 'Search for drink'
-    print_header(false, header, 'home - go home')
+    print_header(false, header, 'bb - go back')
     p('**search drinks by name, entire name not require just first few letters work.', 'cyan')
     user_input = gets.strip
     results = CocktailDb::Drink.search_for_drink(user_input)
-    unless user_input == 'home'
+    unless user_input == 'bb'
       if results.length > 0
-        page_results(results, header + ": '#{user_input}'", true)
+        page_results(results, header + ": '#{user_input}'", 'drink info')
       else
         p('no results found please try again', 'red')
-        search_for_drink
       end
+      search_for_drink
     end
-  end
-
-  def get_drinks
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
   end
 end
--- lib/cocktail_db/drink.rb
+++ lib/cocktail_db/drink.rb
@@ -18,8 +18,28 @@ class CocktailDb::Drink
     new_drink.save
   end
 
-  def self.search_for_drink(drink_name)
-    @@all.select {|drink| drink.name.downcase.start_with?(drink_name.downcase)}
+  def self.get_drinks_by_glass_type(glass_type)
+    @@all.select {|drink| drink.glass == glass_type}
+  end
+
+  def self.get_drinks_by_category(category)
+    @@all.select {|drink| drink.category == category}
+  end
+
+  # def self.find_by_name(drink_name)
+  #   @@all.select {|drink| drink.name == drink_name}.first
+  # end
+
+  def self.search_for_drink(search_query)
+    @@all.select {|drink| drink.name.downcase.start_with?(search_query.downcase)}
+  end
+  
+  def self.get_categories
+    @@all.map {|drink| drink.category}.uniq
+  end
+
+  def self.get_glass_types
+    @@all.map {|drink| drink.glass}.uniq
   end
 
   def save
@@ -37,27 +57,3 @@ class CocktailDb::Drink
   end
 
 end
-
-#strDrink = name
-#strCategory = category
-#strAlcoholic = contains_alcohol
-#strGlass = glass
-#strInstructions = instructions
-#strDrinkThumb = image_link
-
-#   "strDrink"=>"A1", name
-#   "strCategory"=>"Cocktail", category
-#   "strAlcoholic"=>"Alcoholic", contains_alcohol
-#   "strGlass"=>"Cocktail glass", glass
-#   "strInstructions"=>"Pour all ingredients into a cocktail shaker, mix and serve over ice into a chilled glass.", instructions
-#   "strDrinkThumb"=>"https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg", image_link
-
-#   []
-#   "strIngredient1"=>"Gin",
-#   "strIngredient2"=>"Grand Marnier",
-#   "strIngredient3"=>"Lemon Juice",
-#   "strIngredient4"=>"Grenadine",
-#   "strMeasure1"=>"1 3/4 shot ",
-#   "strMeasure2"=>"1 Shot ",
-#   "strMeasure3"=>"1/4 Shot",
-#   "strMeasure4"=>"1/8 Shot",
